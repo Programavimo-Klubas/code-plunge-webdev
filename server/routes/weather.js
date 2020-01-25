ee008cb35d6cbb73533a33e68401a2544a4938dd
Kipras Melnikovas <kipras@kipras.org>
2020-01-25 11:39:56 +0200

    days - pirmadienis, antradienis, treciadienis + JSON
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
+const days = [
+	{
+		title: "Pirmadienis",
+		temp: 12,
+	},
+	{
+		title: "Antradienis",
+		temp: 9,
+	},
+	{
+		title: "Treciadienis",
+		temp: 11,
+	},
+];
+
 router.get("/", function(request, response) {
-	response.send("labas");
+	response.setHeader("Content-type", "application/json");
+	response.send(JSON.stringify(days));
 });
 
 module.exports = router;
