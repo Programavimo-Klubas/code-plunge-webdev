f95017e8b838de5b37092bb6af8c765fb94172e1
Kipras Melnikovas <kipras@kipras.org>
2020-01-25 14:05:44 +0200

    Sukurti "day.js" ir pavyzdžiui naudoti pirmadienį
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- narsykle/src/App.js
+++ narsykle/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Day from "./day"
 
 function App() {
+  return <Day></Day>
 }
 
 export default App;
---
+++ narsykle/src/day.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Day() {
+	return <div>
+		Pirmadienis - 12
+	</div>
+}
+
+export default Day;
