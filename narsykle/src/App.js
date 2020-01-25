55472162becb1b462fb9573620e24b84f43ebe46
Kipras Melnikovas <kipras@kipras.org>
2020-01-25 15:30:28 +0200

    Išsaugoti dienas ir temperatūras React būsenoje
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- narsykle/src/App.js
+++ narsykle/src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Day from "./day"
 
 function App() {
+	const [daysState, setDaysState] = useState([]);
 
 	async function getDays() {
 		const daysText = await fetch("http://localhost:3000/weather");
 		const days = await daysText.json();
 		console.log("days", days);
+		setDaysState(days);
 	}
 
 	useEffect(function() {
