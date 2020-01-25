5f5456fc6086870b3073b89389609e06042991c9
Kipras Melnikovas <kipras@kipras.org>
2020-01-25 15:04:33 +0200

    Naudoti React'o `useEffect`, kad iš serverio gautume orų duomenis, kuriuos išvedam į konsolę (F12)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- narsykle/src/App.js
+++ narsykle/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Day from "./day"
 
 function App() {
+	useEffect(async function () {
+		const daysText = await fetch("http://localhost:3000/weather");
+		const days = await daysText.json();
+		console.log("days", days);
+	});
+
   return (
 	  <div>
 		  <Day title="Pirmadienis" temperature={10} />
